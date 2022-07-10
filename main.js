ba6807a5cd356cac4af9453204dd697ff305b233
Max <104431913+MaxNakhariza@users.noreply.github.com>
2022-07-10 11:10:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+// var myNum = 10,
+//     myStr = 'строка',
+//     myBool = true,
+//     myArr = [1, 2, 3, 4, 5],
+//     myObj = { first: 'First Name', last: 'Last Name' };
+// console.log(myNum);
+// console.log(myString);
+// console.log(myBool);
+// console.log(myArr);
+// console.log(myObj);
+// console.log(myNum);
+
+// var decimal2 = myNum.toFixed(2);
+// console.log(decimal2);
+
+// var i = 222;
+// a = i++;
+// console.log(i, a);
+// b = ++i;
+// console.log(i, b);
+// c = i--;
+// console.log(i, c);
+// d = --i;
+// console.log(i, d);
+
+
+// var myTest = 20;
+// myTest += myNum;
+// console.log(myTest);
+// myTest -= myNum;
+// console.log(myTest);
+// myTest *= myNum;
+// console.log(myTest);
+// myTest /= myNum;
+// console.log(myTest);
+// myTest %= myNum;
+// console.log(myTest);
+
+// var myPi, myRound, myRandom, myPow;
+// myPi = Math.PI;
+// myRound = Math.round(89.279);
+// myRandom = Math.random() * 10;
+// myPow = Math.pow(3, 5);
+// console.log(myPi);
+// console.log(myRound);
+// console.log(myRandom);
+// console.log(myPow);
+
+// var strObj = {
+//     str: 'Мама мыла раму, рама мыла маму',
+//     length: ''
+// };
+// strObj.length = 30;
+// console.log(strObj.str);
+// console.log(strObj.str.length);
+
+// var isRamaPos;
+// isRamaPos = strObj.str.indexOf('рама');
+// console.log(isRamaPos);
+
+// var strReplace;
+// strObj.str = strObj.str.replace('мыла', 'моет');
+// strReplace = strObj.str.replace('мыла', 'держит');
+// strReplace = strReplace.replace('рама', 'Рама');
+// console.log(strReplace);
+
+// var strUpp, strLow;
+// strUpp = obj.str.toUpperCase();
+// strLow = obj.str.toLowerCase();
+// console.log(strUpp);
+// console.log(strLow);
+
+
+
